13dd7cd62908aa8ac38ae5b343fdfef32e8dadba
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-05-16 18:24:40 +0200

    functions to work with api

--- api_site/get_api.py
+++ api_site/get_api.py
@@ -1,3 +1,4 @@
+from typing import List
 import requests
 import json
 
@@ -7,3 +8,10 @@ def api():
         "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium").json()["results"]
     with open("request_api.json", "w") as json_file:
         json.dump(api, json_file)
+
+
+def get_question(num):
+    with open("request_api.json") as file:
+        context = json.load(file)
+    listek = [item["correct_answer"] for item in context]
+    return context[num], listek
