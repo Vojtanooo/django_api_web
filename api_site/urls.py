a6eca2d4a86f015ec9fc504a09e291ed70bff139
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-05-17 19:10:54 +0200

    rename

--- api_site/urls.py
+++ api_site/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('success', views.success, name='success'),
+    path('quiz', views.quiz, name='quiz'),
+    path('', views.starting_page, name='starting-page'),
 ]
