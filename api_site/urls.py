4bf00f4aa76198e8ec754e59a6f8c0cd67c35b68
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-05-22 18:51:15 +0200

    add leaderboard path

--- api_site/urls.py
+++ api_site/urls.py
@@ -1,8 +1,10 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('quiz', views.quiz, name='quiz'),
     path('', views.starting_page, name='starting-page'),
-    path('finish', views.finish_page, name="finish-page")
+    path('finish', views.finish_page, name="finish-page"),
+    path('leaderboard', views.leaderboard, name="leaderboard")
 ]
